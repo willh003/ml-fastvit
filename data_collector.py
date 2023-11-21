f3aa9f0d7969e8b14f1d67fbc58015547fb54f48
Will H <willhu003@gmail.com>
2023-11-21 13:28:00 -0500

    Data_collector update.

--- data_collector.py
+++ data_collector.py
@@ -12,6 +12,9 @@ import os
 import message_filters
 import csv
 
+
+itr = 0
+only_every = 20
 class DataListener:
     def __init__(self, log_dir, cmd_topic='/car/mux/ackermann_cmd_mux/input/teleop', img_topic='/car/car/camera/color/image_raw'):
         self.max_velocity = rospy.get_param("~speed", 2.0)
@@ -22,18 +25,25 @@ class DataListener:
 
 
         self.bridge= CvBridge()
-        self.ts = message_filters.ApproximateTimeSynchronizer([self.obs_sub, self.cmd_sub], 10, .05)
+        self.ts = message_filters.ApproximateTimeSynchronizer([self.obs_sub, self.cmd_sub], 10, .1)
+
         self.ts.registerCallback(self.obs_cb)
         self.data_dir = log_dir
 
         if not os.path.exists(self.data_dir):
             os.makedirs(self.data_dir)
 
-        with open(f'{self.data_dir}/action.csv', 'wb') as f:
+        # with open(f'{self.data_dir}/action.csv', 'wb') as f:
+        #     f.write('t, v, w')
+        with open(f'{self.data_dir}/action.csv', 'w') as f:
             f.write('t, v, w')
 
 
     def obs_cb(self, image, action):
+        print("Callback triggered.")
+        if itr < only_every:
+            itr += 1
+            return
         time = action.header.stamp
         angle = action.drive.steering_angle
         speed = action.drive.speed
@@ -44,14 +54,26 @@ class DataListener:
         if not os.path.exists(img_dir):
             os.makedirs(img_dir)
 
-        cv2.imwrite(os.path.join(img_dir,f'{time}.jpg'), cv_image)
-        with open(f'{self.data_dir}/action.csv', 'wb') as f:
-            f.write(f'{time}, {speed}, {angle}')
+        # cv2.imwrite(os.path.join(img_dir,f'{time}.jpg'), cv_image)
+        # with open(f'{self.data_dir}/action.csv', 'wb') as f:
+        #     f.write(f'{time}, {speed}, {angle}')
+        cv2.imwrite(os.path.join(img_dir, f'{time}.jpg'), cv_image)
+        with open(f'{self.data_dir}/action.csv', 'a') as f:
+            f.write(f'{time.to_sec()}, {speed}, {angle}')
 
-        rospy.sleep(.2)
+        #rospy.sleep(.2)
         
+from pathlib import Path
+if __name__ == "__main__":
+    rospy.init_node('data_listener_node')
+    
+    log_dir = Path("./datacollectortest").resolve()
 
+    data_listener = DataListener(log_dir)
+    print("DataListener instance created")
 
+    print("MUSHR Listener Spinning...")
+    rospy.spin()
 
 
 
